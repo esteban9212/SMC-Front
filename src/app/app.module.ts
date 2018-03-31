e5a10f93c6973426dff2653f88b83f59d8cf646e
Esteban Aguirre Patiño <esteban.aguirre9212@gmail.com>
2018-03-31 18:57:16 -0500

    menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,10 @@ import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { LoginComponent } from './login/login.component';
 import { CreatePlanComponent } from './create-plan/create-plan.component';
+import { HomeComponent } from './home/home.component';
+import { SearchComponent } from './search/search.component';
 import { FormsModule } from '@angular/forms';
+import { RouterModule,Routes } from '@angular/router';
 
 
 
@@ -18,12 +21,19 @@ import { FormsModule } from '@angular/forms';
     AppComponent,
     MenuComponent,
     LoginComponent,
-    CreatePlanComponent
+    CreatePlanComponent,
+    HomeComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {path:'home',component:HomeComponent},
+      {path:'create',component:CreatePlanComponent},
+      {path:'search',component:SearchComponent}
+      ])
   ],
   providers: [ProgramsService,OutcomeService,PlanAssessmentService,],
   bootstrap: [AppComponent]
