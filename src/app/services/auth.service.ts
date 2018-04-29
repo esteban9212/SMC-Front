bcdbe8faa34d71ac79124df9291049400787222a
Juan Camilo Villada Gamboa <jncvg17@hotmail.com>
2018-04-29 11:15:28 -0500

    Actual user and logout done

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from "@angular/core";
 import { Http, Headers, Response } from "@angular/http";
 import 'rxjs/Rx';
 import { Observable } from 'rxjs/Rx';
-import { User } from '../models/user';
 
 @Injectable()
 export class AuthService {
@@ -21,7 +20,8 @@ export class AuthService {
 					const expires_in = response.json().expires_in;
 					const base64Url = token.split('.')[1];
 					const base64 = base64Url.replace('-','+').replace('_','/');
-					return {token:token, decoded: JSON.parse(window.atob(base64)), token_type:token_type, expires_in:expires_in};
+					const user = response.json().user.identification;
+					return {token:token, decoded: JSON.parse(window.atob(base64)), token_type:token_type, expires_in:expires_in, user:user};
 				}
 			)
 			.do(
@@ -29,21 +29,18 @@ export class AuthService {
 					localStorage.setItem('token', tokenData.token);
 					localStorage.setItem('token_type', tokenData.token_type);
 					localStorage.setItem('expires_in', tokenData.expires_in);
-
+					localStorage.setItem('user', JSON.stringify(tokenData.user));
 				});
 	}
 
-	me():Observable<User>{
+
+	logout(){
 		var tokenData = localStorage.getItem('token');
-		return this.http.post('http://127.0.0.1:8000/api/auth/me?token='+tokenData,{},
+		return this.http.post('http://127.0.0.1:8000/api/auth/logout?token='+tokenData,{},
 			{headers: new Headers({'X-Requested-With': 'XMLHttpRequest'})})
-		.map((response:Response)=> response.json())
-		.do(
-				userData => {
-					localStorage.setItem('user', userData);
-				})
-		;
-	}
-
-
+		.map((response:Response)=> {
+			const message = response.json().message;
+			return {message:message};
+		});
+		}
 }
